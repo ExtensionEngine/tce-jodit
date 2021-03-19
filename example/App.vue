f9fb7d51351cfab92b9bb5492f9d65ca729286c2
Zdravko Ćurić <zcuric@gmail.com>
2021-03-19 17:32:19 +0100

    Clean up

--- example/App.vue
+++ example/App.vue
@@ -13,28 +13,11 @@
 export default {
   data: () => ({
     element: {
-      id: 1263,
-      uid: 'd4753387-5cf0-47ce-a0ee-c9dcf0288e88',
       type: 'JODIT_HTML',
       position: 1,
-      contentId: '19d8e9df-f0d5-42bb-9ac6-501dce605a64',
-      contentSignature: '9dae791967ac78f55cfe5afa9c0b280052efc138',
       data: {
-        content: "Lorem Ipsum&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        content: ""
       },
-      meta: {
-        style: {
-          textType: 'quote'
-        }
-      },
-      refs: {},
-      linked: false,
-      detached: false,
-      createdAt: '2020-06-02T08:42:51.110Z',
-      updatedAt: '2020-06-02T08:43:29.767Z',
-      deletedAt: null,
-      repositoryId: 5,
-      activityId: 1286
     }
   })
 };
@@ -77,5 +60,11 @@ body {
   margin-top: 20px;
   padding: 10px;
   border: 2px solid #eee;
+}
+
+.jodit-container:not(.jodit_inline) .jodit-wysiwyg {
+  p {
+    margin: 0;
+  }
 }
 </style>
--- vue.config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  configureWebpack: {
-    output: {
-      libraryExport: 'default'
-    }
-  }
-};
