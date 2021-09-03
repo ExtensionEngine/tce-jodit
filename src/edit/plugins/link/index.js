8c18bcaa37565b60d8243a02d2aa7c52470f45ea
Marin Capeta <marin.capeta@gmail.com>
2021-09-03 17:50:52 +0200

    Focus url input in link popup

--- src/edit/plugins/link/index.js
+++ src/edit/plugins/link/index.js
@@ -1,5 +1,7 @@
 /** @typedef {import('jodit').IJodit} Jodit */
 
+const LINK_INPUT_SELECTOR = 'div.jodit_toolbar_popup-open input[name="url"]';
+
 export default class LinkPlugin {
   static get pluginName() {
     return 'links';
@@ -10,7 +12,13 @@ export default class LinkPlugin {
    */
   init(jodit) {
     jodit.registerCommand('link', {
-      exec: () => jodit.events.fire('click-link-btn', new Event('link')),
+      exec: () => {
+        jodit.events.fire('click-link-btn', new Event('link'));
+        setTimeout(() => {
+          const input = jodit.toolbar.container.querySelector(LINK_INPUT_SELECTOR);
+          if (input) input.focus();
+        });
+      },
       hotkeys: ['ctrl+k', 'cmd+k']
     });
   }
